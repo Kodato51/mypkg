f29fff2c4f6646d5330ef96feafa291cf623239b
Kodato <kodadac85546@outlook.com>
2023-12-29 19:08:49 +0900

    add licence

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,10 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Ryuki Kodato
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch.actions
 import launch.substitutions
 import launch_ros.actions
 
 
-def generate_launch_description( ):
+def generate_launch_description():
 
     talker = launch_ros.actions.Node(
         package='mypkg',
