dc18bfaa71e24b9f862f9274042490dd1258bfc0
Kodato <kodadac85546@outlook.com>
2023-12-29 18:43:36 +0900

    fix talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,10 +3,10 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self,node):
+    def __init__(self, node):
         self.pub = node.create_publisher(Int16, "countup",10)
         self.n = 0
-        node.create_timer(0.5,cb)
+        node.create_timer(0.5,self.cb)
 
         def cd(self):
             msg = Int16()
@@ -14,7 +14,11 @@ class Talker():
             self.pub.publish(msg)
             self.n += 1
 
-rclpy.init()
-node = Node("talker")
-talker = Talker()
-rclpy.spin(node)
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
